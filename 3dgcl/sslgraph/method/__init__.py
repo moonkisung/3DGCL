49ff9ce09eacedeedf6e9a241657ba1ce1530ec6
Kisung Moon <69106214+moonkisung@users.noreply.github.com>
2022-05-31 10:35:10 +0900

    Add files via upload

---
+++ 3dgcl/sslgraph/method/__init__.py
@@ -0,0 +1,12 @@
+from .contrastive.model import Contrastive, GraphCL, GRACE, InfoGraph, MVGRL, NodeMVGRL
+
+__all__ = [
+    'Contrastive',
+    'GraphCL',
+    'GRACE',
+    'InfoGraph',
+    'MVGRL',
+    'NodeMVGRL'
+]
+
+classes = __all__
