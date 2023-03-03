0b479bef82be273c04953637c4453742f0d7b478
Kisung Moon <69106214+moonkisung@users.noreply.github.com>
2023-03-03 16:34:52 +0900

    Update __init__.py

--- dig/threedgraph/method/__init__.py
+++ dig/threedgraph/method/__init__.py
@@ -2,20 +2,10 @@ from .run import run
 from .run_rmse import run_rmse
 from .run_binary import run_binary
 from .schnet import SchNet
-from .dimenetpp import DimeNetPP
-from .spherenet import SphereNet
-from .gin import GIN
-
-from .comenet import ComENet
-
 
 __all__ = [
     'run', 
     'run_rmse', 
     'run_binary', 
-    'SchNet',
-    'DimeNetPP',
-    'SphereNet',
-    'GIN'
-    'ComENet'
+    'SchNet'
 ]
